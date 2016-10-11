47935e5528c353c1b62f29f87237715dc1f9f260
Peter Foot <peter@peterfoot.net>
2016-10-11 16:41:06 +0100

    Documentation updates. DisplayRequest for Win32 applications. New NuGet package

---
+++ Source/InTheHand/System/Display/DisplayRequest.Win32.cs
@@ -0,0 +1,40 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="DisplayRequest.Win32.cs" company="In The Hand Ltd">
+//     Copyright © 2016 In The Hand Ltd. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System.Runtime.InteropServices;
+
+namespace Windows.System.Display
+{
+    public sealed partial class DisplayRequest
+    {
+        private static class NativeMethods
+        {
+            [DllImport("Kernel32", SetLastError = true)]
+            internal static extern EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
+
+            internal enum EXECUTION_STATE : uint
+            {
+                /// <summary>
+                /// Enables away mode.This value must be specified with ES_CONTINUOUS.
+                /// Away mode should be used only by media-recording and media-distribution applications that must perform critical background processing on desktop computers while the computer appears to be sleeping.See Remarks.
+                /// </summary>
+                AWAYMODE_REQUIRED = 0x00000040,
+                /// <summary>
+                /// Informs the system that the state being set should remain in effect until the next call that uses ES_CONTINUOUS and one of the other state flags is cleared.
+                /// </summary>
+                CONTINUOUS = 0x80000000,
+                /// <summary>
+                /// Forces the display to be on by resetting the display idle timer.
+                /// </summary>
+                DISPLAY_REQUIRED = 0x00000002,
+                /// <summary>
+                /// Forces the system to be in the working state by resetting the system idle timer.
+                /// </summary>
+                SYSTEM_REQUIRED = 0x00000001,
+            }
+        }
+    }
+}
--- Source/InTheHand/System/Display/DisplayRequest.cs
+++ Source/InTheHand/System/Display/DisplayRequest.cs
@@ -20,9 +20,21 @@ using System;
 namespace Windows.System.Display
 {
     /// <summary>
-    /// Starts the default app associated with the specified file or URI.
+    /// Represents a display request.
     /// </summary>
-    public sealed class DisplayRequest
+    /// <remarks>
+    /// To conserve power and extend battery life, the system reduces power to the computer if it does not detect any user activity for a certain amount of time.
+    /// Depending on system power settings, the display may first be dimmed, a screen saver may be displayed, and eventually the display may be turned off as the system enters a low-power sleep state.
+    /// <list type="bullet">
+    /// <item><term>Android</term><description>Android 4.4 and later</description></item>
+    /// <item><term>iOS</term><description>iOS 9.0 and later</description></item>
+    /// <item><term>Windows UWP</term><description>Windows 10</description></item>
+    /// <item><term>Windows Store</term><description>Windows 8.1 or later</description></item>
+    /// <item><term>Windows Phone Store</term><description>Windows Phone 8.1 or later</description></item>
+    /// <item><term>Windows Phone Silverlight</term><description>Windows Phone 8.0 or later</description></item>
+    /// <item><term>Windows (Desktop Apps)</term><description>Windows Vista or later</description></item></list>
+    /// </remarks>
+    public sealed partial class DisplayRequest
     {
 #if __ANDROID__
         private bool requested = false;
@@ -47,6 +59,8 @@ namespace Windows.System.Display
             UIApplication.SharedApplication.IdleTimerDisabled = true;
 #elif WINDOWS_PHONE
             Microsoft.Phone.Shell.PhoneApplicationService.Current.ApplicationIdleDetectionMode = Microsoft.Phone.Shell.IdleDetectionMode.Disabled;
+#elif WIN32
+            NativeMethods.SetThreadExecutionState(NativeMethods.EXECUTION_STATE.DISPLAY_REQUIRED | NativeMethods.EXECUTION_STATE.CONTINUOUS);
 #endif
 
         }
@@ -70,6 +84,8 @@ namespace Windows.System.Display
             UIApplication.SharedApplication.IdleTimerDisabled = false;
 #elif WINDOWS_PHONE
             Microsoft.Phone.Shell.PhoneApplicationService.Current.ApplicationIdleDetectionMode = Microsoft.Phone.Shell.IdleDetectionMode.Enabled;
+#elif WIN32
+            NativeMethods.SetThreadExecutionState(NativeMethods.EXECUTION_STATE.CONTINUOUS);
 #endif
         }
     }
