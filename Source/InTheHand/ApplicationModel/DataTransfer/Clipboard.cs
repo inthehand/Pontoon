87e8055bcc942c01ce92fc5b882fb2d01d1b9a1c
Peter Foot <peter@peterfoot.net>
2016-12-14 16:36:24 +0000

    added clipboard platform documentation

--- Source/InTheHand/ApplicationModel/DataTransfer/Clipboard.cs
+++ Source/InTheHand/ApplicationModel/DataTransfer/Clipboard.cs
@@ -27,6 +27,18 @@ namespace InTheHand.ApplicationModel.DataTransfer
     /// <summary>
     /// Gets and sets information from the clipboard object.
     /// </summary>
+    /// <remarks>
+    /// <para/><list type="table">
+    /// <listheader><term>Platform</term><description>Version supported</description></listheader>
+    /// <item><term>Android</term><description>Android 4.4 and later</description></item>
+    /// <item><term>iOS</term><description>iOS 9.0 and later</description></item>
+    /// <item><term>macOS</term><description>OS X 10.7 and later</description></item>
+    /// <item><term>Windows UWP</term><description>Windows 10</description></item>
+    /// <item><term>Windows Store</term><description>Windows 8.1 or later</description></item>
+    /// <item><term>Windows Phone Store</term><description>Windows Phone 8.1 or later</description></item>
+    /// <item><term>Windows Phone Silverlight</term><description>Windows Phone 8.0 or later</description></item>
+    /// <item><term>Windows (Desktop Apps)</term><description>Windows Vista or later</description></item></list>
+    /// </remarks>
     public static partial class Clipboard
     {
 #if __ANDROID__
