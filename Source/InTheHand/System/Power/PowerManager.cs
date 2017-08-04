6df67a1f25293616483b08bbbc7577f362516212
Peter Foot <peter@inthehand.com>
2017-08-04 17:32:54 +0100

    10.2017.8.4

--- Source/InTheHand/System/Power/PowerManager.cs
+++ Source/InTheHand/System/Power/PowerManager.cs
@@ -5,6 +5,7 @@
 // --------------------------------------------------------------------------------------------------------------------
 
 using System;
+using System.Reflection;
 
 namespace InTheHand.System.Power
 {
